c92edd0478a924144e3cd32920970705e96106e6
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-04-02 12:31:21 +0200

    Some tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,27 @@
-var test = require('tape');
+import test from 'tape';
+import { FastArray } from '../src';
 
-test('test', (t) => {
-    t.end()
+test('alloc', t => {
+  const fa = FastArray();
+
+  t.equal(fa.currentLength, 0, 'currentLength of fresh array should be 0');
+  t.equal(fa.maxLength, 1000, 'maxLength should default to 1000');
+  t.equal(fa.arr.length, 1000, 'actual array length should equal maxLength');
+
+  t.end();
+});
+
+test('push', t => {
+  const fa = FastArray({
+    initialSize: 5
+  });
+
+  fa.push(1);
+  fa.push(2);
+  fa.push(3);
+
+  t.deepEqual(fa.arr, [1, 2, 3, 0, 0], 'pushed values should be inserted one after another');
+  t.equal(fa.currentLength, 3, 'currentLength should be equal to number of pushed values');
+
+  t.end();
 });
