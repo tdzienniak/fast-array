049a202a4293112d2e663826b86ab747c90fb944
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2016-04-02 13:24:11 +0200

    More tests

--- test/index.js
+++ test/index.js
@@ -13,7 +13,7 @@ test('alloc', t => {
 
 test('push', t => {
   const fa = FastArray({
-    initialSize: 5
+    initialSize: 5,
   });
 
   fa.push(1);
@@ -25,3 +25,92 @@ test('push', t => {
 
   t.end();
 });
+
+test('pop', t => {
+  const fa = FastArray({
+    initialSize: 5,
+  });
+
+  fa.push(1);
+  fa.push(2);
+  fa.push(3);
+
+  const popped = fa.pop();
+
+  t.equal(popped, 3, 'popped value should be equal to last pushed value');
+  t.deepEqual(fa.arr, [1, 2, 0, 0, 0], 'popped element should be removed from array');
+  t.equal(fa.currentLength, 2, 'currentLength should be changed accordingly');
+
+  t.end();
+});
+
+test('removeAtIndex', t => {
+  const fa = FastArray({
+    initialSize: 5,
+  });
+
+  fa.push(1);
+  fa.push(2);
+  fa.push(3);
+
+  const removed = fa.removeAtIndex(1);
+
+  t.equal(removed, 2, 'method should return removed value');
+  t.deepEqual(fa.arr, [1, 3, 0, 0, 0], 'values should be shifted to close the gap');
+  t.equal(fa.currentLength, 2, 'currentLength should be changed accordingly');
+
+  t.end();
+});
+
+test('unsetAtIndex', t => {
+  const fa = FastArray({
+    initialSize: 5,
+  });
+
+  fa.push(1);
+  fa.push(2);
+  fa.push(3);
+
+  const wasSparseBeforeUnset = fa.sparse;
+  const unset = fa.unsetAtIndex(1);
+
+  t.equal(unset, 2, 'method should return unset value');
+  t.equal(fa.arr[1], fa.fillValue, 'unset index should be filled with `fillValue`');
+  t.deepEqual(fa.arr, [1, 0, 3, 0, 0], 'positions of other elements should not change');
+  t.equal(fa.currentLength, 3, 'currentLength should not change');
+  t.ok(fa.sparse && !wasSparseBeforeUnset, 'array should be marked as sparse');
+
+  t.end();
+});
+
+test('compact', t => {
+  const fa = FastArray({
+    initialSize: 10,
+  });
+
+  fa.push(1);
+  fa.push(2);
+  fa.push(3);
+  fa.push(4);
+  fa.push(5);
+  fa.push(6);
+  fa.push(7);
+  fa.push(8);
+  fa.push(9);
+
+  fa.unsetAtIndex(0);
+  fa.unsetAtIndex(2);
+  fa.unsetAtIndex(4);
+  fa.unsetAtIndex(5);
+  fa.unsetAtIndex(8);
+
+  const wasSparseBeforeCompact = fa.sparse;
+
+  fa.compact();
+
+  t.deepEqual(fa.arr, [2, 4, 7, 8, 0, 0, 0, 0, 0, 0], 'array should be compacted and filled');
+  t.ok(!fa.sparse && wasSparseBeforeCompact, 'array should be dense again');
+  t.equal(fa.currentLength, 4, 'currentLength should be changed accordingly');
+
+  t.end();
+})
